139d1f89a07f4ebae1e3b17bcb5b20a5bdb7abe8
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-01-27 11:52:34 -0300

    copi

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,14 +2,33 @@ const colorPickerBtn = document.querySelector("#color-picker")
 const colorList = document.querySelector(".all-colors")
 const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
 
+const clearAll = document.querySelector(".clear-all")
+
+const copyColor = elem =>{
+    navigator.clipboard.writeText(elem.dataset.color)
+    elem.innerText = "Copiado";
+    setTimeout(() => elem.innerText = elem.dataset.color, 1000)
+}
+
 const showColors = ()=>{
     colorList.innerHTML = pickedColors.map(color => `
     <li class="color">
-    <span class="rect" style="background: ${color}"></span>
-    <div class="value">${color}</div>
+    <span
+     class="rect" 
+     style="background: ${color}; border: 1px solid ${color == "#ffffff" ? "#ccc" : color}"> <!-- adiciona borda caso a cor seja branca -->
+    </span> 
+    <div class="value" data-color="${color}">${color}</div>
     </li>
     `).join("") // gera o li pra cor escolhida e adiciona a Colorlist na tela
+
+
+    // quando clicar na cor escolhida ela sera copiada para o teclado
+    document.querySelectorAll(".color").forEach(li => {
+        li.addEventListener("click", e => copyColor(e.currentTarget.lastElementChild ));
+    })
+
 }
+showColors()
 
 const activeEyeDropper = async () =>{
     try{
@@ -17,9 +36,19 @@ const activeEyeDropper = async () =>{
         const {sRGBHex} = await eyeDropper.open()
         navigator.clipboard.writeText(sRGBHex) // copia no teclado
 
-        pickedColors.push(sRGBHex)
-        localStorage.setItem("picked-colors", JSON.stringify(pickedColors))
-        showColors()
+
+        //adicionar a cor na lista caso ja n tivesse sido inserida
+        if(!pickedColors.includes(sRGBHex)){
+            pickedColors.push(sRGBHex)
+            localStorage.setItem("picked-colors", JSON.stringify(pickedColors))
+            showColors()
+        }else{
+            Swal.fire({
+            confirmButtonText: 'Cor ja Selecionada',
+            confirmButtonColor: '#3085d6'
+        })
+        }
+
     }catch(error){
         console.log(error)
     }
