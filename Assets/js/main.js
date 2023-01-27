88dccd281a6c395bc31f579338f6440236fef8ac
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-01-27 11:21:08 -0300

    colorlist

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,25 @@
 const colorPickerBtn = document.querySelector("#color-picker")
+const colorList = document.querySelector(".all-colors")
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+
+const showColors = ()=>{
+    colorList.innerHTML = pickedColors.map(color => `
+    <li class="color">
+    <span class="rect" style="background: ${color}"></span>
+    <div class="value">${color}</div>
+    </li>
+    `).join("") // gera o li pra cor escolhida e adiciona a Colorlist na tela
+}
 
 const activeEyeDropper = async () =>{
     try{
-        const eyeDropper = new EyeDropper()
+        const eyeDropper = new EyeDropper() // pega o Eyedropper (função do google que usa lupa para pegar cor)
         const {sRGBHex} = await eyeDropper.open()
-        navigator.clipboard.writeText(sRGBHex)
-        console.log( sRGBHex )
+        navigator.clipboard.writeText(sRGBHex) // copia no teclado
+
+        pickedColors.push(sRGBHex)
+        localStorage.setItem("picked-colors", JSON.stringify(pickedColors))
+        showColors()
     }catch(error){
         console.log(error)
     }
